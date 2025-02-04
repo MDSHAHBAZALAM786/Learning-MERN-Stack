ebf722c8ebbbcd88e7bea73c6f9c4e70649ccf66
Md Shahbaz Alam <mdsahbazjhd163@gmail.com>
2025-02-04 21:30:10 +0530

    index

--- REACTWITHVITE/my-react-app/class/Day 17/fetch_weather_API.jsx
+++ REACTWITHVITE/my-react-app/class/Day 17/fetch_weather_API.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export default function App() {
   const [state, setState]=useState()
   useEffect(()=>{
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Patna&appid=77452427fa6e7e242f18d9912a27d30e')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Patna&appid={API}')
     .then((res)=>res.json())
     // .then((data)=>console.log(data.main.temp))
     .then((data)=>setState(data.main.temp))
