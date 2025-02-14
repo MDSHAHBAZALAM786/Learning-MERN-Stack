d29f08f1c96917bbf19af81a34b0c447d756c887
Md Shahbaz Alam <mdsahbazjhd163@gmail.com>
2025-02-14 15:12:06 +0530

    Form

---
+++ REACT+NODE/Client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../Component/Signup'
+import Login from '../Component/Login'
+
+export default function App() {
+  return (
+    <div>
+      <Signup/>
+      <Login/>
+    </div>
+  )
+}
