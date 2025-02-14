391f48f9842fc29a5c09d22279b6ae20dac9c191
Md Shahbaz Alam <mdsahbazjhd163@gmail.com>
2025-02-14 15:31:31 +0530

    Form

--- REACT+NODE/Client/Component/Login.jsx
+++ REACT+NODE/Client/Component/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Login.css'
 
 export default function Login() {
   return (
     <div>
-        <h1>Login</h1>
+        <h1 className='login-heading'>Login</h1>
     </div>
   )
 }
--- REACT+NODE/Client/Component/Signup.jsx
+++ REACT+NODE/Client/Component/Signup.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Signup.css'
 
 export default function Signup() {
   return (
     <div>
-        <h1>Signup</h1>
+        <h1 className='signup-heading'>Signup</h1>
     </div>
   )
 }
